44f73bb6ed97eab9fe60a7685383b8eeda3e70ca
devKomak <komcio96@gmail.com>
2018-04-24 11:15:56 +0200

    ts update

--- src/app/page/content/flights1/flights1.component.ts
+++ src/app/page/content/flights1/flights1.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { UserService } from '../../../users.service';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatSort, MatPaginator, MatTableDataSource } from '@angular/material';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-flights1',
   templateUrl: './flights1.component.html',
