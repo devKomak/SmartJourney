bde82c39a92b785b085828967a3658542aa66f13
Jacek Kurek <jacekkurus@gmail.com>
2018-05-06 16:05:08 +0200

    deleted

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  amadeus_API_KEY: 'AkcyuJvlLDp25A2Ad0mRLVwln7LwgRzM',
+  firebase : {
+    apiKey: 'AIzaSyAyS8viqOPbQdY8XrvgqirJ3KKZIJ6L3t8',
+    authDomain: 'projekt-spa.firebaseapp.com',
+    databaseURL: 'https://projekt-spa.firebaseio.com',
+    projectId: 'projekt-spa',
+    storageBucket: 'projekt-spa.appspot.com',
+    messagingSenderId: '98223056031'
+  }
+};
