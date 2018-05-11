931653e384a115ab40c03481f5b70bfd4423357b
Mateusz Kusal <mati95k@gmail.com>
2018-05-11 19:00:21 +0200

    adding imports

--- src/app/page/content/hotels/hotels.component.ts
+++ src/app/page/content/hotels/hotels.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../../users.service';
+import { Hotel } from '../../../shared/hotel';
+import { Router } from '@angular/router';
 
 
 @Component({
