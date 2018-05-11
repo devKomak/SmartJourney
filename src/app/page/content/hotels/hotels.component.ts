ba76cabcde5cc1fed3f0fa5b4d04e57baf456351
Chivolta <jacekkurus@gmail.com>
2018-05-11 18:53:59 +0200

    Amendments in project

--- src/app/page/content/hotels/hotels.component.ts
+++ src/app/page/content/hotels/hotels.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../../users.service';
-import { Hotel } from '../../../shared/hotel';
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-hotels',
@@ -10,35 +8,13 @@ import { Router } from '@angular/router';
 })
 export class HotelsComponent implements OnInit {
 
-  hotels: Hotel[];
-  show = 5;
-  public started: boolean;
-  public index: number;
-  constructor(private userService: UserService, private router: Router) {
-    this.hotels = this.userService.hotels;
+
+  constructor() {
+
   }
 
   ngOnInit() {
-    this.started = false;
+
   }
 
-  check(hotel: Hotel, index: number){
-    this.started = true;
-    this.index = index;
-    this.userService.addHotel(hotel);
-    this.userService.getPlaces().subscribe(message =>{}, 
-      error => { console.log(error)});
-    this.userService.isPlaces.subscribe(message =>{
-      if (message === true) {  this.router.navigate(['places']); }
-    }
-  )
-  }
-
-  showValue() {
-    this.show += 5;
-  }
-
-  showValueLess() {
-    if (this.show >= 10)  {this.show -= 5; }
-  }
 }
